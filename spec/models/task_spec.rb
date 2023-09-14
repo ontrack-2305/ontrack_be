3ba265cdfdaacdb5b7479fdde0f2c84df1d0be86
Anna Wiley <awiley33@gmail.com>
2023-09-14 18:04:33 -0500

    test: Add task model testing for enum definitions

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe Task, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :category }
     it { should validate_presence_of :time_needed }
+    it { should validate_presence_of :user_id }
+  end
+
+  describe "Enum Definitions" do
+    it { should define_enum_for(:category).with([:rest, :hobby, :chore]) }
+    it { should define_enum_for(:frequency).with([:one_time, :daily, :weekly, :monthly, :annual]) }
   end
 end
