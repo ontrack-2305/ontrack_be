04513d52c834f9484ff56fa91c52c4540c1e34ac
Anna Wiley <awiley33@gmail.com>
2023-09-14 18:12:56 -0500

    refactor: change .with shouldamatchers methods to .with_value to comply with upcoming update

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Task, type: :model do
   end
 
   describe "Enum Definitions" do
-    it { should define_enum_for(:category).with([:rest, :hobby, :chore]) }
-    it { should define_enum_for(:frequency).with([:one_time, :daily, :weekly, :monthly, :annual]) }
+    it { should define_enum_for(:category).with_values([:rest, :hobby, :chore]) }
+    it { should define_enum_for(:frequency).with_values([:one_time, :daily, :weekly, :monthly, :annual]) }
   end
 end
