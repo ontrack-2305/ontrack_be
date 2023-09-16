9d0e9e3d33f9705384b70a096ed7c7e15a7f288c
Parker Boeing <parker.boeing@gmail.com>
2023-09-16 01:33:19 -0600

    wip: Daily task algorithms funcitoning but need to keep track of completion

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,80 @@ class Task < ApplicationRecord
 
   enum category: { rest: 0, hobby: 1, chore: 2 }
   enum frequency: { one_time: 0, daily: 1, weekly: 2, monthly: 3, annual: 4 }
+
+  def self.breakdown_tasks(user_id)
+    @user_tasks = Task.all.where(user_id: user_id)
+    @restful_tasks = @user_tasks.where(category: "rest")
+    @hobby_tasks = @user_tasks.where(category: "hobby")
+    @chore_tasks = @user_tasks.where(category: "chore")
+  end
+
+  def self.good_day_tasks(user_id)
+    Task.breakdown_tasks(user_id)
+    tasks_for_day = []
+    total_task_time = 0
+    while total_task_time < 500
+      chore_task_1 = @chore_tasks.sample
+      chore_task_2 = @chore_tasks.sample
+      hobby_task_1 = @hobby_tasks.sample
+      restful_task_1 = @restful_tasks.sample
+      total_task_time += chore_task_1.time_needed
+      tasks_for_day << chore_task_1
+      total_task_time += chore_task_2.time_needed
+      tasks_for_day << chore_task_2
+      total_task_time += hobby_task_1.time_needed
+      tasks_for_day << hobby_task_1
+      total_task_time += restful_task_1.time_needed
+      tasks_for_day << restful_task_1
+    end
+    tasks_for_day
+  end
+
+  def self.meh_day_tasks(user_id)
+    Task.breakdown_tasks(user_id)
+    tasks_for_day = []
+    total_task_time = 0
+    while total_task_time < 400
+      chore_task_1 = @chore_tasks.sample
+      hobby_task_1 = @hobby_tasks.sample
+      restful_task_1 = @restful_tasks.sample
+      total_task_time += chore_task_1.time_needed
+      tasks_for_day << chore_task_1
+      total_task_time += hobby_task_1.time_needed
+      tasks_for_day << hobby_task_1
+      total_task_time += restful_task_1.time_needed
+      tasks_for_day << restful_task_1
+    end
+    tasks_for_day
+  end
+
+  def self.bad_day_tasks(user_id)
+    Task.breakdown_tasks(user_id)
+    tasks_for_day = []
+    total_task_time = 0
+    while total_task_time < 300
+      hobby_task_1 = @hobby_tasks.sample
+      restful_task_1 = @restful_tasks.sample
+      total_task_time += hobby_task_1.time_needed
+      tasks_for_day << hobby_task_1
+      total_task_time += restful_task_1.time_needed
+      tasks_for_day << restful_task_1
+    end
+    tasks_for_day
+  end
+
+  # build out a def viable_task helper method that checks frequency against when it was last completed,
+  # this will require a migration to add a completed with a date_time stamp
+  #  viable_tasks = []
+  #  one_time_tasks = user_tasks.where(frequency: one_time)
+  #  daily_tasks = user_tasks.where(frequency: daily)
+  #  weekly_tasks = user_tasks.where(frequency: weekly)
+  #  monthly_tasks = user_tasks.where(frequency: monthly)
+  #  annual_tasks = user_tasks.where(frequency: annual)
+  #  viable_tasks << weekly_tasks.where(completion: nil)
+  #  viable_tasks << weekly_tasks.where(:completion < Time.now - 7)
+
+
+
+  # should annual events and holidays come from google calendar api?
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Task, type: :model do
     it { should define_enum_for(:category).with_values([:rest, :hobby, :chore]) }
     it { should define_enum_for(:frequency).with_values([:one_time, :daily, :weekly, :monthly, :annual]) }
   end
+
+  describe "class methods" do
+    it "should return the tasks for a good day" do
+      user1_tasks = FactoryBot.create_list(:task, 20, user_id: 1)
+      user2_tasks = FactoryBot.create_list(:task, 20, user_id: 2)
+
+      Task.good_day_tasks(1)
+      # break day into 12 hours, 2 hour break midday
+      # two chunks of tasks totalling 5 hours each
+      # 2 chores 1 hobby 1 rest
+
+      # create an empty array for objects and an empty array for task time_needed. Add tasks to algorithm until time_needed = 5 hours
+    end
+  end
 end
