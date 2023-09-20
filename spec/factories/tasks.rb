3e73a82c54a5fb959e022ecb66008f695062e779
Parker Boeing <parker.boeing@gmail.com>
2023-09-20 00:17:37 -0600

    feat: Background workers reset skipped tasks at midnight

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -46,16 +46,3 @@ FactoryBot.define do
 end
 
 
-
-# FactoryBot.define do
-#   factory :task do
-#       name { "#{Faker::Verb.base} #{Faker::House.room}" }
-#       category { Task.categories.keys.sample }
-#       mandatory { {Faker::Boolean.boolean} }
-#       event_date { Faker::Time.forward(days: 30, period: :morning) }
-#       frequency { Task.frequencies.keys.sample }
-#       time_needed { Faker::Number.number(digits: 2) }
-#       user_id { Faker::Number.number(digits: 3) }
-#       notes { Faker::Hipster.sentence }
-#   end
-# end
