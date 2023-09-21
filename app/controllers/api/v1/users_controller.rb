c85c58398c7ae3fe5ea1347f476cb0058e60c99e
Artemy Gibson <artemygibson@gmail.com>
2023-09-21 16:57:54 -0700

    Delete controller

--- app/controllers/api/v1/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::UsersController < ApplicationController
-
-  def authenticate
-    user = User.new(params)
-    calendar_events = CalendarEventsFacade.upcoming_events(user)
-
-  end
-end
