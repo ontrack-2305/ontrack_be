053edac6fc9df23dd8fc632b7a9fbc932a4c4859
Artemy Gibson <artemygibson@gmail.com>
2023-09-21 12:56:28 -0700

    Remove hardcoded holidays, change test to be dynamic and still work as time passes

--- spec/requests/api/v1/holidays_request_spec.rb
+++ spec/requests/api/v1/holidays_request_spec.rb
@@ -6,11 +6,26 @@ RSpec.describe "Holiday Service API", :vcr do
       it "holiday service returns next three holidays" do
         get "/api/v1/holidays"
         expect(response.status).to eq(200)
-        parsed_response = JSON.parse(response.body)
-        expect(parsed_response).to eq({"data"=>
-        [{"type"=>"holiday", "attributes"=>{"name"=>"Columbus Day", "date"=>"2023-10-09"}},
-         {"type"=>"holiday", "attributes"=>{"name"=>"Veterans Day", "date"=>"2023-11-10"}},
-         {"type"=>"holiday", "attributes"=>{"name"=>"Thanksgiving Day", "date"=>"2023-11-23"}}]})
+        parsed_response = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed_response).to have_key(:data)
+        expect(parsed_response[:data]).to be_an(Array)
+        expect(parsed_response[:data].count).to eq(3)
+        parsed_response[:data].each do |holiday|
+          expect(holiday).to have_key(:type)
+          expect(holiday[:type]).to eq("holiday")
+
+          expect(holiday).to have_key(:attributes)
+          expect(holiday[:attributes]).to be_a(Hash)
+
+
+          expect(holiday[:attributes]).to have_key(:name)
+          expect(holiday[:attributes][:name]).to be_a(String)
+
+          expect(holiday[:attributes]).to have_key(:date)
+          expect(holiday[:attributes][:date]).to be_a(String)
+          expect(Date.today < holiday[:attributes][:date].to_date).to eq(true)
+          expect(holiday[:attributes][:date]).to match(/\d{4}-\d{2}-\d{2}/)
+        end
       end
     end
   end
