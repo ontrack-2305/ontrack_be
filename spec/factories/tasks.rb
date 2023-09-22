8202087d5ca0647e44c3e5f6900f9b41f5348171
Artemy Gibson <artemygibson@gmail.com>
2023-09-21 20:42:37 -0700

    Remove unneeded lines

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -8,40 +8,6 @@ FactoryBot.define do
     time_needed { Faker::Number.number(digits: 2) }
     user_id { Faker::Number.number(digits: 3) }
     notes { Faker::Hipster.sentence }
-
-    # Define traits for enum (only if you are not using traits_for_enum)
-    trait :rest do
-      category { "rest" }
-    end
-    
-    trait :hobby do
-      category { "hobby" }
-    end
-    
-    trait :chore do
-      category { "chore" }
-    end
-    
-    trait :once do
-      frequency { "once" }
-    end
-    
-    trait :daily do
-      frequency { "daily" }
-    end
-    
-    trait :monthly do
-      frequency { "monthly" }
-    end
-    
-    trait :annual do
-      frequency { "annual" }
-    end
-    
-    trait :weekly do
-      frequency { "weekly" }
-    end
-    # ... similarly for other enums
   end
 end
 
