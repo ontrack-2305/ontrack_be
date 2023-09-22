843f4d7e2951ed5381e8e94f2b588d4abd704578
Artemy Gibson <artemygibson@gmail.com>
2023-09-21 20:44:57 -0700

    Edit test to check all holidays and not one

--- spec/services/holidays_service_spec.rb
+++ spec/services/holidays_service_spec.rb
@@ -5,13 +5,14 @@ RSpec.describe HolidaysService, :vcr do
     search = HolidaysService.new.upcoming_holidays
 
     expect(search).to be_an Array
+    expect(search.count).to eq(12)
 
-    holiday = search.first
+    search.each do |holiday|
+      expect(holiday).to have_key :date
+      expect(holiday[:date]).to be_a String
 
-    expect(holiday).to have_key :date
-    expect(holiday[:date]).to be_a String
-
-    expect(holiday).to have_key :name
-    expect(holiday[:date]).to be_a String
+      expect(holiday).to have_key :name
+      expect(holiday[:name]).to be_a String
+    end
   end
 end
