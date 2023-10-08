2aa9e8df2c7f40452ba318a663186fdb4e336e0a
Parker Boeing <parker.boeing@gmail.com>
2023-10-08 00:54:58 -0600

    WIP: Add redis gem, config for development, and set up chat channel

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :anonymous_user
+
+    def connect
+      self.anonymous_user = SecureRandom.hex(10)
+    end
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_181441) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_30_041609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_181441) do
     t.datetime "completed"
     t.boolean "skipped", default: false
     t.string "image_url"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
