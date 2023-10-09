c781e94f5fd2fb64ebc74ec52a73a1ff177013d3
Parker Boeing <parker.boeing@gmail.com>
2023-10-08 22:06:29 -0600

    fix: Tidy spacing

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -5,16 +5,8 @@ class ChatChannel < ApplicationCable::Channel
 
   def speak(data)
     user_message = data["message"]
-    # ai_response = get_ai_response(user_message)
     response = AiService.new.fetch_chat_response(user_message)
     clean_response = {message: response[:choices][0][:message][:content]}.to_json
     ActionCable.server.broadcast("chat_channel", clean_response)
   end
-
-  private
-  
-  # def get_ai_response(message)
-  #   response = AiService.new.fetch_chat_response(message)
-  #   response[:choices][0][:message][:content]
-  # end
 end
