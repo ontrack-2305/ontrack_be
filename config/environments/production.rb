7addf2bb917366fe4ea6aa5059b85d40e6a0dde0
Parker Boeing <parker.boeing@gmail.com>
2023-10-17 13:26:24 -0600

    fix: Add deployed FE link to allowed origins in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_cable.url = "wss://ontrack-be-a58c9e421d34.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['http://localhost:5000']
+  config.action_cable.allowed_request_origins = ['http://localhost:5000', 'https://ontrack-fe-4b819560ba35.herokuapp.com']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
