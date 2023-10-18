da211e33a04047d3093218fde2a77764514d9ef4
Parker Boeing <parker.boeing@gmail.com>
2023-10-17 21:16:36 -0600

    fix: Sidekiq to work in production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,10 @@
 require 'sidekiq'
 require 'sidekiq-cron'
 
+redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/0'
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: redis_url }
 
   # Load scheduled jobs defined in sidekiq_schedule.yml
   schedule_file = "config/sidekiq_schedule.yml"
@@ -14,5 +16,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0' }
+  config.redis = { url: redis_url }
 end
